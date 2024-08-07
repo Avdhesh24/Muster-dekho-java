191dd590d084c6c4678622f235f7d0eb0aef7c86
Avdhesh singh <rajputavdhesh2000@gmail.com>
2024-08-07 14:42:19 +0530

    Hello

---
+++ varriables.java
@@ -0,0 +1,23 @@
+public class varriables {
+        public static void main(String args[]) {
+
+            String EmpolyeeCompanyName = "Muster Dekho";
+            String EmployeeName = "Avdhesh singh";
+            int EmployeeId = 111;
+            String EmployeeEmailID = "rajputavdhesh24@musterdekho.com" ;
+            long EmpolyeeNumber = 7974604032l;
+            int EmployeeAge = 24;
+            float EmployeeSalary = 400000.00f;
+            char EmpolyeeBloodGroup = 'B';
+
+            System.out.println("Employee Company Name: " + EmpolyeeCompanyName);
+            System.out.println("Empolyee Name: " + EmployeeName);
+            System.out.println("Employee Id: " + EmployeeId);
+            System.out.println("Employee Email ID: " + EmployeeEmailID);
+            System.out.println("Empolyee Number: " + EmpolyeeNumber);
+            System.out.println("Employee Age: " + EmployeeAge);
+            System.out.println("Employee Salary: " + EmployeeSalary);
+            System.out.println("Empolyee Blood Group: " + EmpolyeeBloodGroup);
+
+        }
+    }
