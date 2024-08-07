c3de3d65137e48b9789ba82e4cad5724341b1904
Avdhesh singh <rajputavdhesh2000@gmail.com>
2024-08-07 14:49:34 +0530

    if else condition

---
+++ ifelse.java
@@ -0,0 +1,10 @@
+public class ifelse {
+        public static void main(String args[]){
+            int age = 16;
+            if (age > 18) {
+                System.out.println("Yes Boy you can drive");
+            }else{
+                System.out.println("No Boy you can not drive");
+            }
+        }
+    }
