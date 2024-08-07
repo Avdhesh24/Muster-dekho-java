c01923ea4544bbe5dfc5563af034b83edf867a44
Avdhesh singh <rajputavdhesh2000@gmail.com>
2024-08-07 14:42:31 +0530

    Arithmatic operator(+,-,*,/)

---
+++ ArithmaticOperator.java
@@ -0,0 +1,14 @@
+public class ArithmaticOperator {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+    }
+}
